fc1c773687d7fab48d670ab75a98579ee8071206
KrX3D <KrX3D@users.noreply.github.com>
2026-03-22 23:47:11 +0100

    f

--- components/sd_mmc_card/__init__.py
+++ components/sd_mmc_card/__init__.py
@@ -1,20 +1,13 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
-from esphome import pins, automation
+from esphome import pins
 
 DEPENDENCIES = ["esp32"]
 AUTO_LOAD = ["sensor", "text_sensor"]
 
 sd_ns = cg.esphome_ns.namespace("sd_mmc_card")
 SdMmcCard = sd_ns.class_("SdMmcCard", cg.Component)
-
-# Actions
-WriteFileAction = sd_ns.class_("WriteFileAction", automation.Action)
-AppendFileAction = sd_ns.class_("AppendFileAction", automation.Action)
-DeleteFileAction = sd_ns.class_("DeleteFileAction", automation.Action)
-CreateDirectoryAction = sd_ns.class_("CreateDirectoryAction", automation.Action)
-RemoveDirectoryAction = sd_ns.class_("RemoveDirectoryAction", automation.Action)
 
 CONF_CLK_PIN = "clk_pin"
 CONF_CMD_PIN = "cmd_pin"
@@ -24,16 +17,32 @@ CONF_DATA2_PIN = "data2_pin"
 CONF_DATA3_PIN = "data3_pin"
 CONF_MODE_1BIT = "mode_1bit"
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(SdMmcCard),
-    cv.Required(CONF_CLK_PIN): pins.internal_gpio_output_pin_number,
-    cv.Required(CONF_CMD_PIN): pins.internal_gpio_output_pin_number,
-    cv.Required(CONF_DATA0_PIN): pins.internal_gpio_pin_number,
-    cv.Optional(CONF_DATA1_PIN): pins.internal_gpio_pin_number,
-    cv.Optional(CONF_DATA2_PIN): pins.internal_gpio_pin_number,
-    cv.Optional(CONF_DATA3_PIN): pins.internal_gpio_pin_number,
-    cv.Optional(CONF_MODE_1BIT, default=False): cv.boolean,
-}).extend(cv.COMPONENT_SCHEMA)
+
+def _validate_pins(config):
+    if not config.get(CONF_MODE_1BIT, False):
+        for pin_name in [CONF_DATA1_PIN, CONF_DATA2_PIN, CONF_DATA3_PIN]:
+            if pin_name not in config:
+                raise cv.Invalid(
+                    f"'{pin_name}' is required when mode_1bit is false (4-bit mode). "
+                    f"Set mode_1bit: true to use 1-bit mode with only data0_pin."
+                )
+    return config
+
+
+CONFIG_SCHEMA = cv.All(
+    cv.Schema({
+        cv.GenerateID(): cv.declare_id(SdMmcCard),
+        cv.Required(CONF_CLK_PIN): pins.internal_gpio_output_pin_number,
+        cv.Required(CONF_CMD_PIN): pins.internal_gpio_output_pin_number,
+        cv.Required(CONF_DATA0_PIN): pins.internal_gpio_pin_number,
+        cv.Optional(CONF_DATA1_PIN): pins.internal_gpio_pin_number,
+        cv.Optional(CONF_DATA2_PIN): pins.internal_gpio_pin_number,
+        cv.Optional(CONF_DATA3_PIN): pins.internal_gpio_pin_number,
+        cv.Optional(CONF_MODE_1BIT, default=False): cv.boolean,
+    }).extend(cv.COMPONENT_SCHEMA),
+    _validate_pins,
+)
+
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
@@ -45,9 +54,6 @@ async def to_code(config):
     cg.add(var.set_mode_1bit(config[CONF_MODE_1BIT]))
 
     if not config[CONF_MODE_1BIT]:
-        if CONF_DATA1_PIN in config:
-            cg.add(var.set_data1_pin(config[CONF_DATA1_PIN]))
-        if CONF_DATA2_PIN in config:
-            cg.add(var.set_data2_pin(config[CONF_DATA2_PIN]))
-        if CONF_DATA3_PIN in config:
-            cg.add(var.set_data3_pin(config[CONF_DATA3_PIN]))
+        cg.add(var.set_data1_pin(config[CONF_DATA1_PIN]))
+        cg.add(var.set_data2_pin(config[CONF_DATA2_PIN]))
+        cg.add(var.set_data3_pin(config[CONF_DATA3_PIN]))
